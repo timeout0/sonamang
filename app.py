368aa1322ba46a9b274f1e8f8c26b9a65841b334
timeout0 <131917756+timeout0@users.noreply.github.com>
2024-04-19 15:16:08 +0300

    index veel ei tööta

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import random
+
+words = ["tere", "headaega", "tore", "maailm"]
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    if request.method == "POST":
+        typed_word = request.form.get("typeWord")
+        correct_word = request.form.get("correctWord")
+
+        if typed_word == correct_word:
+            message = "Correct!"
+            word = random.choice(words)
+        else:
+            message = "Incorrect, try Again!"
+            word = correct_word
+
+        
+        title = "Kirjuta " + word.capitalize()
+        return render_template("index.html", word=word, message=message,title=title)
+
+    word = random.choice(word)
+    title = "Kirjuta " + word.capitalize()
+    return render_template("index.html", word=word, title=title)
 
 if __name__ == '__main__':
     app.run(debug=True)
