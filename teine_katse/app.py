3ae459ca8c53145fcf95243f206c508acf59a614
OliverSomera <166364425+OliverSomera@users.noreply.github.com>
2024-05-17 15:15:47 +0300

    Update app.py
    Lisasin playername ja score funktsiooni /home route juurde

--- teine_katse/app.py
+++ teine_katse/app.py
@@ -12,7 +12,14 @@ def home():
 
 
 
-
+@app.route("/home", methods=["POST"])
+def submit(player_name, score):
+    player_name = request.form["input_playername"]
+    score = request.form["input_playerscore"]
+    session['stored_player_name'] = player_name
+    session['stored_score'] = score
+    with open("highscores_and_usernames", "a") as file:
+        file.write("\n", player_name, score)
 
 if __name__=="__main__":
     app.run(debug=True)
