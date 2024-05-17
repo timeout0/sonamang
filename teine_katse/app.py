420709d255b245d4bff941e3fd859cffa7afed3f
timeout0 <131917756+timeout0@users.noreply.github.com>
2024-05-17 14:01:49 +0300

    Create app.py

---
+++ teine_katse/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, session, redirect, url_for
+
+app = Flask(__name__)
+
+
+
+
+@app.route("/", methods=["POST","GET"])
+def home():
+    return render_template("home.html")
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
